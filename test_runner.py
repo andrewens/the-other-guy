f9572eb16fe0ed10ac1818be141793088f7e86a4
andrewens <andrewens4@gmail.com>
2024-11-23 14:19:44 -0700

    Automatically import all test modules and run them in test_runner

--- test_runner.py
+++ test_runner.py
@@ -1,32 +1,41 @@
-from tests import main_test
+import os
+import importlib
 
 tests_directory_name = "tests"
-test_modules = [
-    main_test, # test 0
-]
+
 
 def print_test_summary(failed_tests, passed_tests):
     tests_string1 = "test" if failed_tests == 1 else "tests"
     tests_string2 = "test" if passed_tests == 1 else "tests"
     
-    print(f"{failed_tests} {tests_string1} failed; {passed_tests} {tests_string2} passed")
+    print(f"\n{failed_tests} {tests_string1} failed; {passed_tests} {tests_string2} passed")
 
     if failed_tests <= 0:
         print("All tests passed!")
     
 
 def run():
+    test_modules = {} # string module_name --> <Module>
+    for file_name in os.listdir(tests_directory_name):
+        # get python modules
+        if "__" in file_name:
+            continue
+        
+        filename_without_extension, _ = os.path.splitext(file_name)
+        module = importlib.import_module(tests_directory_name + "." + filename_without_extension)
+
+        test_modules[filename_without_extension] = module
+
     failed_tests = 0
     passed_tests = 0
-    
-    for i in range(len(test_modules)):
-        test = test_modules[i]
+
+    for test_name, test_module in test_modules.items():
         try:
-            test.run()
+            test_module.run()
             passed_tests += 1
 
         except Exception as e:
             failed_tests += 1
-            print(f"Test #{i} threw exception: {e}")
+            print(f"Test '{test_name}' threw exception: {e}")
 
     print_test_summary(failed_tests, passed_tests)
